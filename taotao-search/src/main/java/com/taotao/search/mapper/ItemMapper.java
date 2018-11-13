e0a116a0bd80c87bf46a6b8b36dc02442af8cb64
GanBaby <2806893053@qq.com>
2018-11-13 22:19:37 +0800

    添加了search子模块

---
+++ taotao-search/src/main/java/com/taotao/search/mapper/ItemMapper.java
@@ -0,0 +1,16 @@
+package com.taotao.search.mapper;
+
+import com.taotao.search.pojo.Item;
+
+import java.util.List;
+
+/**
+ * @Author: 甘银道
+ * @Description: java类作用描述
+ * @Date: 2018-11-13 下午 20:23
+ */
+public interface ItemMapper {
+
+    List<Item> getItemList();
+
+}
---
+++ taotao-search/src/main/java/com/taotao/search/service/ItemService.java
@@ -0,0 +1,14 @@
+package com.taotao.search.service;
+
+import com.taotao.common.pojo.TaotaoResult;
+
+/**
+ * @Author: 甘银道
+ * @Description: java类作用描述
+ * @Date: 2018-11-13 下午 20:44
+ */
+public interface ItemService {
+
+    TaotaoResult importAllItems();
+
+}
